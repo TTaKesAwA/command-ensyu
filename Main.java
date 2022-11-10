ca8557e7b6a7a8b9e37556dd68ecfd24f776166b
takesawa <t.takesawa.sys22@morijyobi.ac.jp>
2022-11-10 13:14:51 +0900

    正しいコミットメッセージ

--- Main.java
+++ Main.java
@@ -4,11 +4,17 @@ public class Main{
 	public static void main(String[] args){
 		Scanner stdln = new Scanner(System.in);
 
-		System.out.println("身長を入力してください。");
+		System.out.println("一人目の身長を入力してください。");
 		double height = stdln.nextDouble();
-		System.out.println("体重を入力してください。");
+		System.out.println("一人目の体重を入力してください。");
 		double weight = stdln.nextDouble();
+		System.out.println("二人目の身長を入力してください。");
+		double height2 = stdln.nextDouble();
+		System.out.println("二人目の体重を入力してください。");
+		double weight2 = stdln.nextDouble();
 		double bmi = weight/Math.pow(height,2);
-	System.out.println("BMI値は"+(Math.floor(bmi*100)/100)+"です。");
+		double bmi2 = weight2/Math.pow(height2,2);
+	System.out.println("一人目のBMI値は"+(Math.floor(bmi*100)/100)+"です。");
+	System.out.println("二人目のBMI値は"+(Math.floor(bmi2*100)/100)+"です。");
 	}
 }
